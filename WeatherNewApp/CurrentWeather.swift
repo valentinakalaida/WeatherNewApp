bf291c4615097f3a1541221ba2f08d193282d37f
Valentina Kalaida <valen2302@ukr.net>
2020-04-02 18:12:51 +0300

    first commit

---
+++ WeatherNewApp/CurrentWeather.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeather.swift
+//  WeatherNewApp
+//
+//  Created by Валентина Калайда on 30.03.2020.
+//  Copyright © 2020 Валентина Калайда. All rights reserved.
+//
+
+import Foundation
